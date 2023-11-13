8e032660f17254f1d1f74c501b91af7039ca9528
aliashtab77 <109904632+aliashtab77@users.noreply.github.com>
2023-11-14 03:21:21 +0330

    جلسه چهارم

---
+++ 04/Primitivetypes.java
@@ -0,0 +1,31 @@
+// boolean data type
+boolean x = true;
+boolean y = false;
+// byte data type
+// -128 to 127
+byte n = 23;
+
+// short data type
+// -32,768 to 32,767
+short m = 34;
+
+// int data type
+// -2,147,483,648 to 2,147,483,647
+int o = -123213123;
+// long data type
+// -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+long qw = 234234234L;
+
+// float data type
+float vdh = 7276.232f;
+
+// double data type
+double njd = 277728.2372333d;
+
+
+// char data type
+char a = '\u0000';
+char b = 'A';
+
+
+
